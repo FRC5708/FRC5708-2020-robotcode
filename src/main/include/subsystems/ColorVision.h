805fb677af4127416b83535cacc8b443394bce82
Team 5708 Laptop <github@uni-nets.com>
2020-02-02 12:21:27 -0500

    New Colorvision

---
+++ src/main/include/subsystems/ColorVision.h
@@ -0,0 +1,15 @@
+#ifndef COLOR_VISION
+#define COLOR_VISION
+
+#include <frc2/command/SubsystemBase.h>
+#include "rev/ColorSensorV3.h"
+#include "RobotMap.h"
+
+class ColorVision : protected frc2::SubsystemBase {
+private:
+    rev::ColorSensorV3 colorSensor{(frc::I2C::Port) I2CPORT};
+public:
+    ColorVision();
+    void Periodic();
+};
+#endif
