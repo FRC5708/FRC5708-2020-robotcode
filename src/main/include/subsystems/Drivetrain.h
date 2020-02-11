3389587af72bb841325df5b9df85ca28510a274b
Owen Treib <otreib@localhost.localdomain>
2020-02-10 19:13:50 -0500

    Change Drivetrain Motors to CAN Falcon 500 (Requires Pheonix on Compiling Computer)

--- src/main/include/subsystems/Drivetrain.h
+++ src/main/include/subsystems/Drivetrain.h
@@ -3,6 +3,7 @@
 #include <frc2/command/SubsystemBase.h>
 #include <frc/Victor.h>
 #include <frc/Encoder.h>
+#include <ctre/Phoenix.h>
 
 #include "DIOMaps.h"
 
@@ -10,10 +11,10 @@ class Drivetrain : protected frc2::SubsystemBase {
 	private:
 	frc::Encoder* const leftEncoder = new frc::Encoder(leftEncoderChannel[0],leftEncoderChannel[1]);
 	frc::Encoder* const rightEncoder = new frc::Encoder(rightEncoderChannel[0],rightEncoderChannel[1]);
-	frc::SpeedController* const FLMotor = new frc::Victor(frontLeftMotorChannel);
-	frc::SpeedController* const FRMotor = new frc::Victor(frontRightMotorChannel);
-	frc::SpeedController* const BLMotor = new frc::Victor(backLeftMotorChannel);
-	frc::SpeedController* const BRMotor = new frc::Victor(backRightMotorChannel);
+	TalonSRX FLMotor = {frontLeftMotorChannel};
+	TalonSRX FRMotor = {frontRightMotorChannel};
+	TalonSRX BLMotor = {backLeftMotorChannel};
+	TalonSRX BRMotor = {backRightMotorChannel};
 	public:
 	Drivetrain();
 	void Drive(double left, double right);
