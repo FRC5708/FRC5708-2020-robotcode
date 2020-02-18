d41a78470c272044939b32abec3b99e9f1cdd6b5
Max Mueggler <maximumsomething@gmail.com>
2020-02-17 19:48:30 -0500

    Bang bang

--- src/main/cpp/subsystems/Manipulators.cpp
+++ src/main/cpp/subsystems/Manipulators.cpp
@@ -1,5 +1,9 @@
 #include "subsystems/Manipulators.h"
 
+// Talons measure speed in encoder ticks per 100 ms
+constexpr double speedMultiplier = 256.0*0.1;
+
+
 Manipulators::Manipulators() {
 }
 
@@ -10,13 +14,13 @@ void Manipulators::ConfigShooterMotor(TalonSRX* theMotor) {
 	theMotor->SetSensorPhase(true);
 
 	/* set the peak and nominal outputs */
-	theMotor->ConfigNominalOutputForward(0.1, kTimeoutMs);
-	theMotor->ConfigNominalOutputReverse(-0.1, kTimeoutMs);
+	theMotor->ConfigNominalOutputForward(0.3, kTimeoutMs);
+	theMotor->ConfigNominalOutputReverse(-0.3, kTimeoutMs);
 	theMotor->ConfigPeakOutputForward(1, kTimeoutMs);
 	theMotor->ConfigPeakOutputReverse(-1, kTimeoutMs);
 	/* set closed loop gains in slot0 */
 	theMotor->Config_kF(kPIDLoopIdx, 0, kTimeoutMs);
-	theMotor->Config_kP(kPIDLoopIdx, 0.22, kTimeoutMs);
+	theMotor->Config_kP(kPIDLoopIdx, 100000000, kTimeoutMs);
 	theMotor->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
 	theMotor->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
 }
@@ -24,10 +28,10 @@ void Manipulators::ConfigShooterMotor(TalonSRX* theMotor) {
 void Manipulators::ManipInit() {
     ConfigShooterMotor(SRMotor);
 	ConfigShooterMotor(SLMotor);
+	SRMotor->SetInverted(true);
 }
 
-// Talons measure speed in encoder ticks per 100 ms
-double speedMultiplier = 256.0*0.1;
+
 void Manipulators::SetShooterWheels(int speed) {
 	speed *= speedMultiplier;
     SRMotor->Set(TalonSRXControlMode::Velocity,speed);
