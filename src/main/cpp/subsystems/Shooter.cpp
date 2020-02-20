2a892dd3e72d717a33be818d04ce4409fee939e2
Max Mueggler <maximumsomething@gmail.com>
2020-02-20 07:39:15 -0500

    Maybe fixed shooter?

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -12,8 +12,11 @@ Shooter::Shooter(){
 void Shooter::ConfigureMotor(TalonSRX* theMotor) {
 	theMotor->ConfigFactoryDefault();
     /* first choose the sensor */
-	theMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, kTimeoutMs);
+	theMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, kTimeoutMs);
 	theMotor->SetSensorPhase(true);
+
+	// Must be in coast mode for bang-bang control to function
+	theMotor->SetNeutralMode(NeutralMode::Coast);
 
 	/* set the peak and nominal outputs */
 	theMotor->ConfigNominalOutputForward(0.3, kTimeoutMs);
@@ -22,6 +25,7 @@ void Shooter::ConfigureMotor(TalonSRX* theMotor) {
 	theMotor->ConfigPeakOutputReverse(-1, kTimeoutMs);
 	/* set closed loop gains in slot0 */
 	theMotor->Config_kF(kPIDLoopIdx, 0, kTimeoutMs);
+	// Very large P value has effect of applying minimum power when going faster than set speed, and maximum when going slower.
 	theMotor->Config_kP(kPIDLoopIdx, 100000000, kTimeoutMs);
 	theMotor->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
 	theMotor->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
