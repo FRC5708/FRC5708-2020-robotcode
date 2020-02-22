b87538c29448067062c08898c4b342348de6edfe
Max Mueggler <maximumsomething@gmail.com>
2020-02-22 11:13:06 -0500

    WIP shooter stuff

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -19,7 +19,7 @@ void Shooter::ConfigureMotor(WPI_TalonSRX* theMotor) {
 	theMotor->ConfigFactoryDefault();
     /* first choose the sensor */
 	theMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, kTimeoutMs);
-	theMotor->SetSensorPhase(false);
+	theMotor->SetSensorPhase(true);
 
 	// Must be in coast mode for bang-bang control to function
 	theMotor->SetNeutralMode(NeutralMode::Coast);
@@ -27,12 +27,12 @@ void Shooter::ConfigureMotor(WPI_TalonSRX* theMotor) {
 	/* set the peak and nominal outputs */
 	theMotor->ConfigNominalOutputForward(0.3, kTimeoutMs);
 	theMotor->ConfigNominalOutputReverse(-0.3, kTimeoutMs);
-	theMotor->ConfigPeakOutputForward(0.55, kTimeoutMs);
-	theMotor->ConfigPeakOutputReverse(-0.55, kTimeoutMs);
+	theMotor->ConfigPeakOutputForward(1, kTimeoutMs);
+	theMotor->ConfigPeakOutputReverse(0, kTimeoutMs);
 	/* set closed loop gains in slot0 */
 	theMotor->Config_kF(kPIDLoopIdx, 0, kTimeoutMs);
 	// Very large P value has effect of applying minimum power when going faster than set speed, and maximum when going slower.
-	theMotor->Config_kP(kPIDLoopIdx, 100000000, kTimeoutMs);
+	theMotor->Config_kP(kPIDLoopIdx, 100, kTimeoutMs);
 	theMotor->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
 	theMotor->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
 }
@@ -46,6 +46,8 @@ void Shooter::setShooterWheels(double speed){
 		speed *= speedMultiplier;
 		rightShooterMotor->Set(TalonSRXControlMode::Velocity,speed);
 		leftShooterMotor->Set(TalonSRXControlMode::Velocity,speed);
+		//rightShooterMotor->Set(TalonSRXControlMode::PercentOutput, 1);
+		//leftShooterMotor->Set(TalonSRXControlMode::PercentOutput, 1);
 	}
 }
 void Shooter::setLoader(loader position){
