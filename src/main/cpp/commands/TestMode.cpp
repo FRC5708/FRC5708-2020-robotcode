34efb847f2221ffd8b9f56818257f359c8854c27
Mothebad <mothebad@gmail.com>
2020-03-06 20:38:05 -0500

    added a theroretically working test of the joystick Y value, and continued (but did not finish) the shuffleboard intigration.

--- src/main/cpp/commands/TestMode.cpp
+++ src/main/cpp/commands/TestMode.cpp
@@ -1,6 +1,9 @@
-#include "commands/TestMode.h"
 #include <frc/shuffleboard/Shuffleboard.h>
-#include <chrono>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+#include "commands/TestMode.h"
+#include "Control.h"
+
 
 
 namespace TestingCommands {
@@ -8,6 +11,7 @@ namespace TestingCommands {
     Intake* intake = Intake::getIntake();
     Odometry* odometry = Odometry::getOdometry();
     Shooter* shooter = Shooter::getShooter();
+    frc::XboxController* controller = Control::controller;
     std::chrono::steady_clock test_clock;
     std::chrono::steady_clock::time_point test_command_start_time;
     namespace DrivetrainTestingCommands {
@@ -21,13 +25,35 @@ namespace TestingCommands {
             drivetrain->Drive(0.3, 0.3);
         }
         bool TestDriveForward::IsFinished() {
-            return  test_clock.now() - test_command_start_time > std::chrono::seconds(2);
+            return test_clock.now() - test_command_start_time > std::chrono::seconds(2);
         }
-        
         void TestDriveForward::End(bool interrupted) {
             drivetrain->Drive(0, 0);
         }
-        
+        TestJoystickYValue::TestJoystickYValue() {
+            AddRequirements(drivetrain);
+        }
+        void TestJoystickYValue::Initialize() {
+            test_clock.now() = test_command_start_time;
+        }
+        void TestJoystickYValue::Execute() {
+            if ((test_clock.now() - test_command_start_time) % std::chrono::milliseconds(50) == std::chrono::milliseconds(0)) {
+                std::cout << controller->GetY(frc::GenericHID::JoystickHand::kLeftHand) << std::endl;
+            }
+        }
+        bool TestJoystickYValue::IsFinished() {
+            return test_clock.now() - test_command_start_time > std::chrono::seconds(5);
+        }
+        void TestJoystickYValue::End(bool interrupted) {
+            drivetrain->Drive(0, 0);
+        }   
     }
 }
+/*void SOMETHING::Initialize() {
+    drivetrain_sendablechooser.SetDefaultOption("None", 0);
+    drivetrain_sendablechooser.AddOption("ACTIVATES MOTORS - Test driving motors forward", 1);
+    drivetrain_sendablechooser.AddOption("ACTIVATES MOTORS - Test the Y value of the Xbox controller", 2);
+    frc::SmartDashboard::PutData("Drivetrain Tests", &drivetrain_sendablechooser);
+}
 //SmartDashboard::PutData("ElevatorMove: up", new ElevatorMove(2.7));
+*/
--- src/main/include/commands/TestMode.h
+++ src/main/include/commands/TestMode.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <iostream>
 #include <chrono>
+#include <frc/XboxController.h>
 
 #include <frc2/command/CommandBase.h>
 #include <frc2/command/CommandHelper.h>
 #include <frc2/command/SequentialCommandGroup.h>
+#include <frc/smartdashboard/SendableChooser.h>
+
 #include "subsystems/Drivetrain.h"
 #include "subsystems/Intake.h"
 #include "subsystems/Shooter.h"
@@ -17,7 +21,9 @@ namespace TestingCommands{
     extern Shooter* shooter;
     extern std::chrono::steady_clock test_clock;
     extern std::chrono::steady_clock::time_point test_command_start_time;
+    extern frc::XboxController* controller;
     namespace DrivetrainTestingCommands{
+        extern frc::SendableChooser<int> drivetrain_sendablechooser;
         class TestDriveForward : public frc2::CommandHelper<frc2::CommandBase,TestDriveForward>{
             void Initialize() override;
             void Execute() override;
@@ -25,22 +31,29 @@ namespace TestingCommands{
             void End(bool interrupted) override;
         public:
             TestDriveForward();
-
-
         };
         class TestJoystickYValue : public frc2::CommandHelper<frc2::CommandBase,TestJoystickYValue>{
-
+            void Initialize() override;
+            void Execute() override;
+            bool IsFinished() override;
+            void End(bool interrupted) override;
+        public:
+            TestJoystickYValue();
         };
     }
     namespace IntakeTestingCommands{
-
+        extern frc::SendableChooser<int> intake_sendablechooser;
+        //if state is intake, the first threee should get passed directly, fourth should get held at top, fith should stp internaly in intake
+        //after that point force intake should force motors to run no matter what
     }
     namespace OdometryTestingCommands{
+        extern frc::SendableChooser<int> odometry_sendablechooser;
         class ReturnPose2D : public frc2::CommandHelper<frc2::CommandBase,ReturnPose2D>{
 
         };
     }
     namespace ShooterTestingCommands{
+        extern frc::SendableChooser<int> shooter_sendablechooser;
         class ReturnShooterMotorPowers : public frc2::CommandHelper<frc2::CommandBase,ReturnShooterMotorPowers>{
 
         };
@@ -49,20 +62,15 @@ namespace TestingCommands{
     using namespace DrivetrainTestingCommands;
     
     class DrivetrainTests : public frc2::CommandHelper<frc2::SequentialCommandGroup,DrivetrainTests> {
-        //WHEN IMPLEMETING THIS IN Drivetrain.cpp PUT THIS IN A FUNCTION THERE :)
-        //AddRequirements(drivetrain);
-        //AddCommands();
+        
     };
     class IntakeTests : public frc2::CommandHelper<frc2::SequentialCommandGroup,IntakeTests> {
-        //AddRequirements(intake);
-        //AddCommands();
+
     };
     class OdometryTests : public frc2::CommandHelper<frc2::SequentialCommandGroup,OdometryTests>{
-        //AddRequirements(odometry);
-        //AddCommands();
+
     };
     class ShooterTests : public frc2::CommandHelper<frc2::SequentialCommandGroup,ShooterTests>{
-        //AddRequirements(shooter);
-        //AddCommands();
+
     };
 }
